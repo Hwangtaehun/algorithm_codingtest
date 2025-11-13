7b0d70b8bca2ab9acddf62771ba45838744e12ed
Hwangtaehun <r283@naver.com>
2025-11-13 20:25:04 +0900

    문제28번은 다시 풀어보기
    어려움

---
+++ question/number28/number28/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+static int g_data[10][10];
+static int paper[6] = { 0, 5, 5, 5, 5, 5 };
+static int minCase = 0x0FFFFFFF;
+
+void backtracking(int cnt);
+
+int main()
+{
+	return 0;
+}
