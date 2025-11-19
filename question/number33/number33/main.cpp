82d6d5b03ae7785b6d49e1f570e4e1322ad82193
Hwangtaehun <r283@naver.com>
2025-11-19 19:26:05 +0900

    문제33 다시 풀어보기

--- question/number33/number33/main.cpp
+++ question/number33/number33/main.cpp
@@ -2,3 +2,25 @@
 #include <vector>
 
 using namespace std;
+
+static int cnt;
+static vector<int> g_data;
+
+int main()
+{
+	int num;
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> num >> cnt;
+	g_data.resize(num);
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> g_data[i];
+	}
+
+	return 0;
+}
